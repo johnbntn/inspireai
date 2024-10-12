7a5aee2629d055e88c82b2026aadf23655b85996
John Benton <jbenton4494@gmail.com>
2024-10-12 15:59:34 -0400

    actually done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
 import markov as m
 def main():
-    
+
+    wordCount = random.randint(5, 15)
     #Create chain
     with open('trainingData.txt', 'r', encoding='utf-8') as file:
         text = file.readlines()
-    markov = m.Markov().buildChain(text)
+    markov = m.Markov().buildChain(wordCount, text)
 
     #Call intro thing
 
