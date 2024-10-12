dc067ce9fe548deab1ba4ddfc79ac00b406dd483
John Benton <jbenton4494@gmail.com>
2024-10-12 17:50:42 -0400

    more cleaning

--- clean.py
+++ clean.py
@@ -8,7 +8,7 @@ with open('input.txt', 'r', encoding='utf-8') as file:
     text = file.readlines()
 
 # Process each line and apply the regex substitution
-with open('output.txt', 'w', encoding='utf-8') as file:
+with open('trainingData.txt', 'w', encoding='utf-8') as file:
     for line in text:
         modified_line = re.sub(pattern, r'\1', line)
         file.write(modified_line)
