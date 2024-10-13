da05353dcab422b46da3846203c1556c0fc61912
John Benton <jbenton4494@gmail.com>
2024-10-12 21:30:37 -0400

    repeats

---
+++ inspire.py
@@ -0,0 +1,23 @@
+import random
+import markov as m
+import ui
+import time
+def main():
+
+    status = True
+    ui.intro()
+
+    with open('trainingData.txt', 'r', encoding='utf-8') as file:
+        text = file.readlines()
+
+    while status == True:
+        wordCount = random.randint(5, 15)
+        quote = "\"" + m.Markov().buildChain(wordCount, text) + "\""
+        print(quote)
+        time.sleep(2)
+        print("\nI:Hope that helped, would you like to see another? (yes,no)")
+        response = input()
+        if response == "no" or response == "No":
+            status = False
+
+main()
